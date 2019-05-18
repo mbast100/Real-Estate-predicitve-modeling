7244bbb617d63182dc745bbbf5c1e9d6ac71005b
Mark Bastawros <mbast.amin97@gmail.com>
2019-05-18 16:26:35 -0400

    Update dataPipeline.py

--- Data processing/dataPipeline.py
+++ Data processing/dataPipeline.py
@@ -6,14 +6,20 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
 from cleaningData import encode_text_oneHot
 from sklearn.pipeline import FeatureUnion
+from cleaningData import oneShotEncoder
+from StratifiedTest import housing
+from sklearn.preprocessing import OneHotEncoder
 
+data = housing
 numerical_attributes = list(numerical_data)
-categoical_attributes = "ocean_proximity"
+categoical_attributes = ["ocean_proximity"]
 
-numerical_pipeline = Pipeline([('Selector',DfSelector(numerical_data)),('imputer', SimpleImputer(strategy ="median")),('attribs_adder', CombinedAttributes()),('standarized_saler',StandardScaler)])
+numerical_pipeline = Pipeline([('Selector',DfSelector(numerical_attributes)),('imputer', SimpleImputer(strategy ="median")),('attribs_adder', CombinedAttributes()),('standarized_saler',StandardScaler())])
 
-categoical_pipeline = Pipeline([('selector',DfSelector(categoical_attributes)),("encoder", encode_text_oneHot(categoical_attributes))])
+categoical_pipeline = Pipeline([('selector',DfSelector(categoical_attributes)),("encoder", OneHotEncoder())])
 
+pipeline = FeatureUnion(transformer_list=[("numerical_pipeline", numerical_pipeline),("catergorical_pipeline",categoical_pipeline)])
 
-#pipeline = FeatureUnion(transformer_list=[("numerical_pipeline", numerical_pipeline),("categorical_pipeline",categoical_pipeline)])
+final = pipeline.fit_transform(data)
 
+print(final.shape)
